2da1c80bf568f5cf3d8dc7fa7559f7a080bc8e19
Valverde Daniel <valverdedaniel44@gmail.com>
2019-07-23 17:38:46 -0600

    initial commit for byterights v1 basic

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://danny:danny123@ds253537.mlab.com:53537/brtest',
+  googleClientID: '270938475112-aafnj56p7l6qsgg8mfjvinvosl3en8e3.apps.googleusercontent.com',
+  googleClientSecret: 'wy2xVUoleUYALPHvuHHOHxe5'
+}
